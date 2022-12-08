659643b2fb98d9c66a24694d6ee53a67f2c5ac6e
max-sliva <persimon@inbox.ru>
2022-12-08 19:52:43 +0500

    added some files

--- src/main/kotlin/ObjectForFerre.kt
+++ src/main/kotlin/ObjectForFerre.kt
@@ -1,2 +1,2 @@
-data class ObjectForFerre(val num: Int, val samplePlace: String, val sampleData1: String, val sampleData2: String, val sand: Double, val dust: Double, val mud: Double)
+data class ObjectForFerre(val num: Int, val samplePlace: String, val depth: String, val sampleNumber: String, val sand: Double, val dust: Double, val mud: Double)
 
